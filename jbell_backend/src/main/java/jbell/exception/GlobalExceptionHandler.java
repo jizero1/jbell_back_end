37655293559f9ef27d8b5efaf663d56326823b72
jizero1 <jizero4101@naver.com>
2026-01-12 12:40:03 +0900

    [turtleNeck-CJY] ApiResponse 파일 수정 detail : ApiResponse 파일 수정

--- jbell_backend/src/main/java/jbell/exception/GlobalExceptionHandler.java
+++ jbell_backend/src/main/java/jbell/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package jbell.exception;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
